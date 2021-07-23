522f68c94ede75297999f09951768ddd5f55fd87
Oliver Schmidt <oliver.t.schmidt@gmail.com>
2021-07-23 10:41:14 -0700

    added code & example of frequewncy-time analysis
    @article{nekkanti2020frequency,
      title={Frequency-time analysis, low-rank reconstruction and denoising of turbulent flows using SPOD},
      author={Nekkanti, Akhil and Schmidt, Oliver T},
      journal={arXiv preprint arXiv:2011.03644},
      year={2020}
    }

---
+++ example_7_FTanalysis.m
@@ -0,0 +1,57 @@
+%% EXAMPLE 7: Frequency-time analysis.
+%
+%   Reference:
+%     [1] A. Nekkanti, O. T. Schmidt, Frequency-time analysis, low-rank reconstruction and denoising of turbulent flows using SPOD, 
+%         arXiv:2011.03644, 2020
+%
+% A. Nekkanti (aknekkan@eng.ucsd.edu), O. T. Schmidt (oschmidt@ucsd.edu)
+% Last revision: 18-Dec-2020
+
+clc, clear variables
+addpath('utils')
+disp('Loading the entire test database might take a second...')
+load(fullfile('jet_data','jetLES.mat'),'p','x','r','dt');
+
+%% SPOD
+%   We will use standard parameters: a Hann window of length 256 and 50%
+%   overlap.
+nFFT  	= 128;
+nOvlp 	= floor(nFFT/2);
+weight 	= trapzWeightsPolar(r(:,1),x(1,:));
+window	= hann(nFFT);
+
+[L,P,f] = spod(p,window,weight,nOvlp,dt);
+
+figure 
+loglog(f,L)
+xlabel('frequency'), ylabel('SPOD mode energy')
+
+%% Frequency-time analysis.
+%   We will compute the mode expansion coefficients of the first 3 modes
+%   using a windowing function and weights consistent with the SPOD.
+nModes          = 3;
+a               = tcoeffs(p,P,window,weight,nModes);
+
+%% Visualize the results.
+nt  = size(p,1);
+t   = (1:nt)*dt;
+
+figure
+
+subplot(2,1,1)
+pcolor(t,f,abs(squeeze(a(:,:,1)))); shading interp
+daspect([100 1 1])
+title(['frequency-time diagram (first mode, ' num2str(sum(L(:,1))/sum(L(:))*100,'%3.1f') '% of energy)'])
+xlabel('time'), ylabel('frequency'), caxis([0 0.75].*caxis)
+
+subplot(2,1,2)
+pcolor(t,f,squeeze(abs(sum(a,3)))); shading interp
+daspect([100 1 1])
+title(['frequency-time diagram (sum of first ' num2str(nModes) ' modes, ' num2str(sum(sum(L(:,1:3)))/sum(L(:))*100,'%3.1f') '% of energy)'])
+xlabel('time'), ylabel('frequency'), caxis([0 0.75].*caxis)
+
+%% Alternatively, plot for pre-multiplied spectra
+% pcolor(t,f,abs(squeeze(a(:,:,1)).*f')); shading interp
+% set(gca,'YScale','log')
+% daspect([50 1 1])
+% title('premultiplied frequency-time diagram')
---
+++ tcoeffs.m
@@ -0,0 +1,84 @@
+function [a] = tcoeffs(X,P,window,weight,nModes)
+%TCOEFFS Continuously-discrete temporal expansion coefficients of SPOD
+%modes
+%   [A] = TCOEFFS(X,P,WINDOW,WEIGHT,NMODES) returns the
+%   continuously-discrete temporal SPOD mode expansion coefficients of the
+%   leading NMODES modes. P is the data matrix of SPOD modes returned by
+%   SPOD. X, WINDOW and WEIGHT are the same variables as for SPOD. If no
+%   windowing function is specified, a Hann window of length WINDOW will
+%   be used. If WEIGHT is empty, a uniform weighting of 1 is used.
+%
+%   Reference:
+%     [1] A. Nekkanti, O. T. Schmidt, Frequency-time analysis, low-rank
+%     reconstruction and denoising of turbulent flows using spectral POD,
+%     arXiv:2011.03644, 2020
+%
+% A. Nekkanti (aknekkan@eng.ucsd.edu), O. T. Schmidt (oschmidt@ucsd.edu)
+% Last revision: 18-Dec-2020
+
+dims        = size(X);
+nt          = dims(1);
+nGrid       = prod(dims(2:end));
+
+window = window(:); weight = weight(:);
+
+% default window size and type
+if length(window)==1
+    window  = hammwin(window);
+end
+
+nDFT        = length(window);
+winWeight   = 1/mean(window);
+
+% inner product weight
+if isempty(weight)
+    weight  = ones(nGrid,1);
+end
+
+X           = reshape(X,nt,nGrid);
+X           = X-mean(X,1);              % subtrct mean
+
+ndims       = size(P);
+P           = permute(P,[1 length(ndims) 2:length(ndims)-1]);
+if isreal(X)
+    nFreq   = ceil(nDFT/2)+1;
+    P       = reshape(P,ceil(nDFT/2)+1,ndims(end),nGrid);
+else
+    nFreq   = nDFT;
+    P       = reshape(P,nDFT,ndims(end),nGrid);
+end
+
+weight     = reshape(weight,1,nGrid);
+% padding the data with zeroes at both ends
+X          = [zeros(ceil(nDFT/2),nGrid); X; zeros(ceil(nDFT/2),nGrid);];
+a          = zeros(nFreq,nt,nModes);
+winCorr_fac= nDFT/winWeight;
+disp(' ')
+disp('Calculating expansion coefficients')
+disp('------------------------------------')
+for i=1:nt
+    X_blk               = fft(X(i:i+nDFT-1,:).*window);
+    X_blk               = X_blk(1:nFreq,:);
+    X_blk(2:end-1,:)    = 2*X_blk(2:end-1,:);
+    % correction for windowing of zero-padded data
+    if (i<ceil(nDFT/2)-1)
+        corr 	= sqrt(winCorr_fac/sum(window(ceil(nDFT/2)-i+1:nDFT)));
+    elseif (i>=nt-ceil(nDFT/2)+1)
+        corr	= sqrt(winCorr_fac/sum(window(1:nt+ceil(nDFT/2)-i)));
+    else
+        corr 	= 1;
+    end
+    for j=1:nFreq
+        for l=1:nModes
+            a(j,i,l)    = corr*(squeeze(P(j,l,:)))'*(weight.*X_blk(j,:)).';
+        end
+    end
+    disp(['time ' num2str(i) '/' num2str(nt)])
+end
+end
+
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+function [window] = hammwin(N)
+%HAMMWIN Standard Hamming window of lenght N
+    window = 0.54-0.46*cos(2*pi*(0:N-1)/(N-1))';
+end
